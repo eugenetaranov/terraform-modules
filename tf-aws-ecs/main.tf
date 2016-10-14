9ff53499ab4ee7325498d133ea9f23697b6254f0
Eugene Taranov <eugene@taranov.me>
2016-10-15 02:58:00 +0300

    Update main.tf

--- tf-aws-ecs/main.tf
+++ tf-aws-ecs/main.tf
@@ -1,11 +1,11 @@
 resource "aws_iam_role" "ecs_host_role" {
   name               = "ecs_host_role"
-  assume_role_policy = "${path.module}/policies/ecs-role.json"
+  assume_role_policy = "${file(${path.module}/policies/ecs-role.json)}"
 }
 
 resource "aws_iam_role_policy" "ecs_instance_role_policy" {
   name   = "ecs_instance_role_policy"
-  policy = "${path.module}/policies/ecs-instance-role-policy.json"
+  policy = "${file(${path.module}/policies/ecs-instance-role-policy.json)}"
   role   = "${aws_iam_role.ecs_host_role.id}"
 }
 
@@ -16,7 +16,7 @@ resource "aws_iam_role" "ecs_service_role" {
 
 resource "aws_iam_role_policy" "ecs_service_role_policy" {
   name   = "ecs_service_role_policy"
-  policy = "${path.module}/policies/ecs-service-role-policy.json"
+  policy = "${file(${path.module}/policies/ecs-service-role-policy.json)}"
   role   = "${aws_iam_role.ecs_service_role.id}"
 }
 
