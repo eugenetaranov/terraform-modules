77abe0fd0458f03dedc892a5093b6740ed2e3bbe
Eugene Taranov <eugene@taranov.me>
2016-10-15 03:09:32 +0300

    Update main.tf

--- tf-aws-ecs/main.tf
+++ tf-aws-ecs/main.tf
@@ -1,22 +1,22 @@
 resource "aws_iam_role" "ecs_host_role" {
   name               = "ecs_host_role"
-  assume_role_policy = "${file("${path.module}/ecs-role.json")}"
+  assume_role_policy = "${file("${path.module}/policies/ecs-role.json")}"
 }
 
 resource "aws_iam_role_policy" "ecs_instance_role_policy" {
   name   = "ecs_instance_role_policy"
-  policy = "${file("${path.module}/ecs-instance-role-policy.json")}"
+  policy = "${file("${path.module}/policies/ecs-instance-role-policy.json")}"
   role   = "${aws_iam_role.ecs_host_role.id}"
 }
 
 resource "aws_iam_role" "ecs_service_role" {
   name               = "ecs_service_role"
-  assume_role_policy = "${file("${path.module}/ecs-role.json")}"
+  assume_role_policy = "${file("${path.module}/policies/ecs-role.json")}"
 }
 
 resource "aws_iam_role_policy" "ecs_service_role_policy" {
   name   = "ecs_service_role_policy"
-  policy = "${file("${path.module}/ecs-service-role-policy.json")}"
+  policy = "${file("${path.module}/policies/ecs-service-role-policy.json")}"
   role   = "${aws_iam_role.ecs_service_role.id}"
 }
 
